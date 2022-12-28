c0200831731c8c183c9a9b8db9722838e2706fed
vgonisanz <vgonisanz@gmail.com>
2022-12-28 21:50:44 +0100

    Added la tirada del circulo

--- tarot_fortune/bin/fortune.py
+++ tarot_fortune/bin/fortune.py
@@ -8,7 +8,22 @@ app = typer.Typer()
 @app.command()
 def single(cards_path: str = "tarot_fortune/data/es/cards.json", card_type: str = ""):
     tarot_picker = TarotCardPicker(cards_path)
-    title, card, meaning = tarot_picker.choose_card_single(card_type)
+    title, card, meaning = tarot_picker.pick_a_card(card_type)
+    tarot_picker.print_result(title, card, meaning)
+
+
+@app.command()
+def draw_past_present_future(cards_path: str = "tarot_fortune/data/es/cards.json", card_type: str = ""):
+    tarot_picker = TarotCardPicker(cards_path)
+    title, card, meaning = tarot_picker.draw_past_present_future(card_type)
+    tarot_picker.print_result(title, card, meaning)
+
+
+@app.command()
+def draw_circle_for_depth(cards_path: str = "tarot_fortune/data/es/cards.json",
+                          card_type: str = "", num_cards: int = 5):
+    tarot_picker = TarotCardPicker(cards_path)
+    title, card, meaning = tarot_picker.draw_circle_for_depth(card_type, num_cards)
     tarot_picker.print_result(title, card, meaning)
 
 
--- tarot_fortune/picker.py
+++ tarot_fortune/picker.py
@@ -15,7 +15,7 @@ class TarotCardPicker:
             cards = data["cards"]
             return cards
 
-    def choose_card_single(self, card_type: str = ""):
+    def pick_a_card(self, card_type: str = ""):
         """
         Select randomly a card, up or down.
         Return two lists, cards and meanings.
@@ -30,6 +30,47 @@ class TarotCardPicker:
         else:
             cards.append(random.choice(self.__cards))
         meanings.append(random.choice(["up", "down"]))
+        return title, cards, meanings
+
+    def draw_past_present_future(self, card_type: str = ""):
+        """
+        Select randomly 3 cards, up or down.
+        Return two lists, cards and meanings.
+        Linking the past, the present and the future.
+        """
+        title = "Pasado, presente y futuro"
+        cards = []
+        meanings = []
+
+        for _ in range(3):
+            if card_type:
+                cards_filtered = [card for card in self.__cards if card["type"] == card_type]
+                cards.append(random.choice(cards_filtered))
+            else:
+                cards.append(random.choice(self.__cards))
+            meanings.append(random.choice(["up", "down"]))
+        return title, cards, meanings
+
+    def draw_circle_for_depth(self, card_type: str = "", num_cards: int = 5):
+        """
+        Select randomly a number of cards between 5 and 8, inclusive.
+        Return two lists, cards and meanings.
+        """
+        title = "Círculo para la profundidad"
+        cards = []
+        meanings = []
+
+        if num_cards < 5 or num_cards > 8:
+            raise ValueError("El número de cartas debe estar entre 5 y 8, incluyendo ambos extremos.")
+
+        for _ in range(num_cards):
+            if card_type:
+                cards_filtered = [card for card in self.__cards if card["type"] == card_type]
+                cards.append(random.choice(cards_filtered))
+            else:
+                cards.append(random.choice(self.__cards))
+            meanings.append(random.choice(["up", "down"]))
+
         return title, cards, meanings
 
     def print_result(self, title, cards, meanings):
